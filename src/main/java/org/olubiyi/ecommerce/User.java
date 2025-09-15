8c83f7bfdef9cef4db6e9724b0dcbd6e781dc225
Olubiyitaiwo <olubiyipaul01@gmail.com>
2025-09-15 13:00:05 +0100

    create user uniqe id

--- src/main/java/org/olubiyi/ecommerce/User.java
+++ src/main/java/org/olubiyi/ecommerce/User.java
@@ -1,4 +1,11 @@
 package org.olubiyi.ecommerce;
 
+import lombok.Data;
+
+@Data
 public class User {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
 }
--- src/main/java/org/olubiyi/ecommerce/UserController.java
+++ src/main/java/org/olubiyi/ecommerce/UserController.java
@@ -1,4 +1,30 @@
 package org.olubiyi.ecommerce;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/Api/Users")
+    public List<User> getAllUsers(){
+        return userService.fetchAllUsers();
+    }
+
+    @GetMapping("/Api/Users/{id}")
+    public User getUsers(@PathVariable Long id){
+        return userService.fetchUserById(id);
+    }
+
+    @PostMapping("/Api/Users")
+    public String createUser(@RequestBody User user){
+        userService.addUser(user);
+        return "user added successfully";
+    }
 }

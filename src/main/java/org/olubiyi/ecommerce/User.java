49a21d2d1b30e3ce90db5505ae46596ec96fbe65
Olubiyitaiwo <olubiyipaul01@gmail.com>
2025-09-17 13:39:46 +0100

    integrating database

--- src/main/java/org/olubiyi/ecommerce/User.java
+++ src/main/java/org/olubiyi/ecommerce/User.java
@@ -1,10 +1,22 @@
 package org.olubiyi.ecommerce;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-public class User {
+@NoArgsConstructor
+//@AllArgsConstructor   Not really important for JPA
+//@Entity(name = "user_table")  How to Rename a table
 
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
